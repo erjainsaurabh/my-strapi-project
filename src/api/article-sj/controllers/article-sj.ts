9d0fd6abde6b6576a9757f6147b7e18749aec1ff
Saurabh Jain <saurabh@jain.com>
2024-11-06 15:14:59 -0500

    committing the change related to arcticle file

---
+++ src/api/article-sj/controllers/article-sj.ts
@@ -0,0 +1,7 @@
+/**
+ * article-sj controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::article-sj.article-sj');
---
+++ src/api/article-sj/routes/article-sj.ts
@@ -0,0 +1,7 @@
+/**
+ * article-sj router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::article-sj.article-sj');
